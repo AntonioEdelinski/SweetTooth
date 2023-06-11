e81ec807b96ee906edf27c66cfa876623eb9e1a9
AntonioEdelinski <95490631+AntonioEdelinski@users.noreply.github.com>
2023-06-11 20:23:50 +0200

    Update index.js

--- sweettooth/src/router/index.js
+++ sweettooth/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import login from '../views/login.vue'
+import register from '../views/register.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +19,23 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import ('../views/login.vue'),
+    meta: {
+      blockUser: true,
+    },
+  },
+
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import ('../views/register.vue'),
+    meta: {
+      blockUser: true,
+    },
   }
 ]
 
