21a58f182b5f156ea681e2a929b027937588a391
AntonioEdelinski <95490631+AntonioEdelinski@users.noreply.github.com>
2023-07-06 16:21:23 +0200

    sidebar.vue

---
+++ sweettooth/src/views/sidebar.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list>
+        <v-list-item>
+          <v-list-item-action>
+            <v-btn text large @click="navigateTo('/login')">Login</v-btn>
+          </v-list-item-action>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-action>
+            <v-btn text large @click="navigateTo('/register')">Register</v-btn>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        drawer: false
+      };
+    },
+    methods: {
+      navigateTo(route) {
+        this.$router.push(route);
+      }
+    }
+  };
+  </script>
+  
