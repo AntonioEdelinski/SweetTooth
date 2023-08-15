3e121594fa153dc29638ea58f411aa547d7c5b8b
AntonioEdelinski <95490631+AntonioEdelinski@users.noreply.github.com>
2023-08-15 13:51:27 +0200

    Update firebase.js

--- sweettooth/src/firebase.js
+++ sweettooth/src/firebase.js
@@ -1,4 +1,14 @@
 // Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app'
+import { doc, addDoc, getDoc, setDoc, getDocs, getCount, getFirestore, collection } from 'firebase/firestore/lite'
+import { getAuth, updatePassword, signOut, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendPasswordResetEmail, confirmPasswordReset, beforeAuthStateChanged, onAuthStateChanged } from 'firebase/auth'
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -7,15 +17,36 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDjzm33y2QgViqTHeD374HuRQ8JFFBw38A",
-  authDomain: "sweettooth-84f3c.firebaseapp.com",
-  projectId: "sweettooth-84f3c",
-  storageBucket: "sweettooth-84f3c.appspot.com",
-  messagingSenderId: "590524353123",
-  appId: "1:590524353123:web:59ca5272fde7e1dbd68656",
-  measurementId: "G-0RYQTRHS32"
+  apiKey: "AIzaSyCmWuEgqEyBGIKj3-lW28LHHaB5WAqT6VM",
+  authDomain: "sweettooth-2d285.firebaseapp.com",
+  projectId: "sweettooth-2d285",
+  storageBucket: "sweettooth-2d285.appspot.com",
+  messagingSenderId: "127867249055",
+  appId: "1:127867249055:web:0338f0a1353a8770795daa",
+  measurementId: "G-9K473610FE"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+const db = getFirestore(app);
+
+export { app, auth, storage,
+  getAuth,
+	db,
+  doc,
+  getDoc,
+	setDoc,
+  addDoc,
+	createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+  onAuthStateChanged,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  signOut,
+  updatePassword
+
+}
