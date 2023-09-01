892667b007ba766f41a5c5076f68a6d8405bc74d
AntonioEdelinski <95490631+AntonioEdelinski@users.noreply.github.com>
2023-09-01 16:37:41 +0200

    Update main.js

--- sweettooth/src/main.js
+++ sweettooth/src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import vuetify from './plugins/vuetify'
-
+import vuetify from './plugins/vuetify.js'
+import croppa from './plugins/croppa.js'
 Vue.config.productionTip = false
 
 new Vue({
   router,
   vuetify,
+  croppa,
   render: h => h(App)
 }).$mount('#app')
