b1688c3c1ece9b1163c229e3a856dd10a6042b8f
AntonioEdelinski <95490631+AntonioEdelinski@users.noreply.github.com>
2023-09-11 12:43:27 +0200

    Update HomeView.vue

--- sweettooth/src/views/HomeView.vue
+++ sweettooth/src/views/HomeView.vue
@@ -1,42 +1,47 @@
 <template>
-    <div class="home-container">
-        <h1>Desserts</h1>
-    
-<div v-for="recipe in recipes" :key="recipe.id">
-    <h2>{{ recipe.name }}</h2>
-    <p>{{ recipe.ingredients }}</p>
-    <p>{{ recipe.description }}</p>
-    <img :src="recipe.imageUrl" alt="Recipe" />
-</div>
-    </div>
+  <div class="home-container">
+    <h1>Desserts</h1>
+
+    <!-- Loop through your recipes and display a card for each one -->
+    <recipe-card
+      v-for="recipe in recipes"
+      :key="recipe.id"
+      :recipe="recipe"
+    ></recipe-card>
+  </div>
 </template>
+
 <script>
-import { db } from '../firebase.js'
+import RecipeCard from "@/components/RecipeCarda.vue"; 
+import { db } from '../firebase.js';
 import { collection, getDocs } from 'firebase/firestore/lite';
 
 export default {
-    name:"HomeView",
-    data() {
-        return {
-            recipes: [],
-        };
-    },
-    methods: {
-        async fetchRecipes () {
-            const recipesCollection = collection(db, "recipes");
-            const querySnapshot = await getDocs (recipesCollection);
+  name: "HomeView",
+  components: {
+    RecipeCard,
+  },
+  data() {
+    return {
+      recipes: [],
+    };
+  },
+  methods: {
+    async fetchRecipes() {
+      const recipesCollection = collection(db, "recipes");
+      const querySnapshot = await getDocs(recipesCollection);
 
-            this.recipe = querySnapshot.docs.map((doc) => doc.data());
-        },
+      this.recipes = querySnapshot.docs.map((doc) => doc.data());
+
+      console.log("Fetched recipes:", this.recipes);
     },
-    created () {
-        this.fetchRecipes();
-    },
+  },
+  created() {
+    this.fetchRecipes();
+  },
 };
 </script>
 
 <style scoped>
-.home-container{
-   padding: 20px;
-}
+
 </style>
